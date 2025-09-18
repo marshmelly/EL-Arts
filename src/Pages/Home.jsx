b20bf2da7f9e29b22165a040ac9fc01297b86b7b
marshmelly <rolfensonfobbs05@gmail.com>
2025-09-18 15:40:26 +0300

    Second Commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div className='home-component'>Home</div>
+    <div className='home-component'>
+      <div className="models">1</div>
+      <div className="rigs">2</div>
+      <div className="illustration">3</div>
+      <div className="fan-art">4</div>
+    </div>
   )
 }
 
